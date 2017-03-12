b89efa8be7cba984270b728d461101d2bb47c066
h0iby <hoiby@outlook.com>
2017-03-12 01:01:50 +0100

    structure

--- app.js
+++ app.js
@@ -11,13 +11,19 @@ var express = require('express'),
 var serviceCallback = function(data){
 	var jsonData = JSON.parse(data),
 		paths = [],
+		htmlPath = "/__resources/html",
+		htmlErrorsPath = htmlPath + "/pages/errors",
+		htmlTemplatesPath = htmlPath + "/templates",
+		htmlMenuPath = htmlPath + "/templates/menu",
+		htmlPagesPath = htmlPath + "/pages",
+		htmlDatabasePath = htmlPath + "/pages/database",
 		html = "";
 
-	var templateMenu = path.join(__dirname+'/src/_default/db-menu.html'),
-		templateMenuLevel1 = path.join(__dirname+'/src/_default/db-menu-level1.html'),
-		templateMenuLevel1Children = path.join(__dirname+'/src/_default/db-menu-level1-children.html'),
-		templateMenuLevel2 = path.join(__dirname+'/src/_default/db-menu-level2.html'),
-		templatePage = path.join(__dirname+'/src/_default/index.html');
+	var templateMenu = path.join(__dirname+''+htmlMenuPath+'/db-menu.html'),
+		templateMenuLevel1 = path.join(__dirname+''+htmlMenuPath+'/db-menu-level1.html'),
+		templateMenuLevel1Children = path.join(__dirname+''+htmlMenuPath+'/db-menu-level1-children.html'),
+		templateMenuLevel2 = path.join(__dirname+''+htmlMenuPath+'/db-menu-level2.html'),
+		templatePage = path.join(__dirname+''+htmlTemplatesPath+'/index.html');
 
 	var fileMenu = fs.readFileSync(templateMenu).toString(),
 		fileMenuLevel1 = fs.readFileSync(templateMenuLevel1).toString(),
@@ -25,23 +31,54 @@ var serviceCallback = function(data){
 		fileMenuLevel2 = fs.readFileSync(templateMenuLevel2).toString(),
 		filePage = fs.readFileSync(templatePage).toString();
 
+	var htmlReplacements = function(input, main, alias, path, robots){
+		var output = input;
+		if(alias == null || alias == undefined){ alias = ""; }
+		if(path == null || path == undefined){ path = ""; }
+		if(robots == null || robots == undefined){ robots = "noindex follow"; }
+
+		output = output.replace("#MAIN#", main);
+		output = output.replace("#MENUITEM#", alias);
+		output = output.replace("#MENUENDPOINT#", path);
+		if(robots != ""){ output = output.replace("#ROBOTS#", robots); }
+
+		return output;
+	}
+
+	var replaceFunction = function(input, replaceFrom, replaceTo){
+		var output = input.replace(replaceFrom, replaceTo);
+
+		if(input.indexOf(replaceFrom) > -1){
+			replaceFunction(output, replaceFrom, replaceTo);
+		} else {
+			return output;
+		}
+	}
+
+
 
 	//create the menu structure for database
 	jsonData.forEach(function(item, i){
-		if(item.render && item.show == '/'){
-			var current = "";
-			if(item.children){ current = fileMenuLevel1Children; }
-			else { current = fileMenuLevel1; }
-			current = current.replace("#MENUNAME#", item.menu).replace("#MENUPATH#", item.path);
-			fileMenu = fileMenu.replace("#LEVEL1#", current + " #LEVEL1#");
-		} else if(item.show != ''){
-			var current = fileMenuLevel2;
-			current = current.replace("#MENUNAME#", item.menu).replace("#MENUPATH#", item.path);
-			fileMenu = fileMenu.replace("#LEVEL2#", current);
+		var name = item.menu;
+		var alias = item.alias;
+		if(item.render){
+			if(item.show == '/'){
+				fileMenu = fileMenu.replace("#LEVEL2#", "");
+				var current = fileMenuLevel1;
+				if(item.children){ current = fileMenuLevel1Children }
+				current = current.replace("#MENUNAME#", name).replace("#MENUCLASS#", alias).replace("#MENUPATH#", item.path);
+				fileMenu = fileMenu.replace("#LEVEL1#", current + " #LEVEL1#");
+			} else if(item.show != ''){
+				var current = fileMenuLevel2;
+				current = current.replace("#MENUNAME#", name).replace("#MENUCLASS#", alias).replace("#MENUPATH#", item.path);
+				fileMenu = fileMenu.replace("#LEVEL2#", current + "#LEVEL2#");
+			}
 		}
 	});
-	fileMenu = fileMenu.replace("#LEVEL1#", "");
+	fileMenu = fileMenu.replace("#LEVEL1#", "").replace("#LEVEL2#", "");
 	html = filePage.replace("#DBMENU#", fileMenu);
+
+
 
 
 
@@ -51,10 +88,11 @@ var serviceCallback = function(data){
 	jsonData.forEach(function(item, i){
 		if(item.render){
 			var current = html;
-			var htmlPath = item.path.replace(":id", "id");
-			var templateMain = path.join(__dirname+'/src/_templates/' + htmlPath + ".html");
+			var alias = item.alias;
+			var currentPath = item.path.replace(":id", "id");
+			var templateMain = path.join(__dirname+''+htmlDatabasePath+'/endpoints/' + currentPath + ".html");
 			var fileMain = fs.readFileSync(templateMain).toString();
-			current = current.replace("#MAIN#", fileMain);
+			current = htmlReplacements(current, fileMain, alias, item.path, "");
 			paths.push([item, current]);
 		}
 	});
@@ -63,32 +101,53 @@ var serviceCallback = function(data){
 	  controller.setup(app, controllerName[0], controllerName[1], controllerName[2]);
 	});
 
-
-
 	// create endpoint for database
 	app.get('/database', (req, res) => {
 		var current = html;
-		var templateMain = path.join(__dirname+'/src/_templates/_database.html');
+		var templateMain = path.join(__dirname+''+htmlDatabasePath+'/database.html');
 		var fileMain = fs.readFileSync(templateMain).toString();
-		current = current.replace("#MAIN#", fileMain);
+		current = htmlReplacements(current, fileMain, null, null, "index follow");
 		res.send(current);
 	});
-
-
 
 	// create endpoint for home
 	app.get('/', (req, res) => {
 		var current = html;
-		var templateMain = path.join(__dirname+'/src/_templates/_default.html');
+		var templateMain = path.join(__dirname+''+htmlPagesPath+'/default.html');
 		var fileMain = fs.readFileSync(templateMain).toString();
-		current = current.replace("#MAIN#", fileMain);
+		current = htmlReplacements(current, fileMain, null, null, "index follow");
 		res.send(current);
 	});
 
-
-
 	// create static endpoints for assets and such
 	app.use(express.static("src"));
+
+	// error handling
+	app.use(function(req, res, next) {
+		var err = new Error('Not Found');
+		err.status = 404;
+		next(err);
+	});
+	// error handling - development
+	if (app.get('env') === 'development') {
+		app.use(function(err, req, res, next) {
+			res.status(err.status || 500);
+			var current = html;
+			var templateMain = path.join(__dirname+''+htmlErrorsPath+'/'+err.status+'.html');
+			var fileMain = fs.readFileSync(templateMain).toString();
+			current = htmlReplacements(current, fileMain, null, null, null);
+			res.send(current);
+		});
+	}
+	// error handling - production
+	app.use(function(err, req, res, next) {
+		res.status(err.status || 500);
+		var current = html;
+		var templateMain = path.join(__dirname+''+htmlErrorsPath+'/'+err.status+'.html');
+		var fileMain = fs.readFileSync(templateMain).toString();
+		current = htmlReplacements(current, fileMain, null, null, null);
+		res.send(current);
+	});
 
 
 
@@ -100,6 +159,9 @@ var serviceCallback = function(data){
 }
 
 
+
+
+
 // service request - with callback when done.
 var serviceRequest = function(){
 	var xhr = new XMLHttpRequest();
@@ -109,6 +171,8 @@ var serviceRequest = function(){
 	xhr.open("GET", "http://localhost:80/endpoints");
 	xhr.send();
 }
+
+
 
 // start service request to get endpoints from webservice
 serviceRequest();
--- routes/database.js
+++ routes/database.js
@@ -6,7 +6,28 @@ var path = require('path'),
 module.exports = {
     setup: (app, item, html) => {
 		app.get("/database" + item.path, (req, res) => {
-			res.send(html);
+
+			var serviceCallback = function(response){
+				if(response == "[]"){
+					robots = 'noindex follow';
+				} else {
+					robots = 'index follow';
+				}
+				html = html.replace('#ROBOTS#', robots);
+				res.send(html);
+			}
+
+			var serviceRequest = function(){
+				var xhr = new XMLHttpRequest();
+				xhr.onreadystatechange = function(err) {
+					if (this.readyState === 4) { serviceCallback(this.responseText); }
+				};
+				xhr.open("GET", "http://localhost:80" + item.path.replace(":id", req.params.id));
+				xhr.send();
+			}
+			serviceRequest();
+
+
 		});
 
     }
